4a514f256818189e01ca49701bbe000544b3b58f
Vincent Lin <vincentlin83@berkeley.edu>
2023-01-16 22:58:51 -0800

    changed linkedin link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,7 @@ function Footer() {
         <div>
           <div class='social-icons'>
             <a class='social-icon-link email' href='mailto:vincentlin83@berkeley.edu' target='_new'><i class='fa fa-envelope' /></a>
-            <a class='social-icon-link linkedin' href='https://www.linkedin.com/in/vincent-lin-15169a186/' target='_new'><i class='fab fa-linkedin' /></a>
+            <a class='social-icon-link linkedin' href='https://www.linkedin.com/in/vincentlin83/' target='_new'><i class='fab fa-linkedin' /></a>
             <a class='social-icon-link Instagram' href='https://www.instagram.com/vincent_lin83' target="_new"><i class='fab fa-instagram' /></a>
           </div>
         </div>
