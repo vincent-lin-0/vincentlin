1fbbbb7b43faf5f5335118a1a69c08aeed955067
Vincent Lin <vincentlin83@berkeley.edu>
2023-01-16 23:18:52 -0800

    added project HabitsTracker

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -12,8 +12,14 @@ function Cards() {
                     <CardItem
                         src={require('../images/SnackMatch.png')}
                         text='SnackMatch: Tinder with Resturants, matching you and your indecisive friends to your new favorite restaurants using the Yelp API'
-                        label='React/MongoDB'
+                        label='ReactJS/MongoDB'
                         path='https://github.com/vincent-lin-0/SnackMatch'
+                    />
+                    <CardItem
+                        src={require('../images/habitsTracker.webp')}
+                        text='HabitsTracker: Tracking your everyday good habits to keep you motivated for your new year resolutions. Uses MySQL to store data'
+                        label='JavaScript/SQL'
+                        path='https://github.com/vincent-lin-0/HabitsTracker'
                     />
                     <CardItem
                         src={require('../images/gitlet.png')}
@@ -46,7 +52,7 @@ function Cards() {
                 <ul className='cards__items'>
                     <CardItem
                         src={require('../images/housing.jpeg')}
-                        text='Housing Prices: Built a linear regression model that predicts the housing prices using feature engineering and cross validation'
+                        text='Housing Prices: A linear regression model that predicts the housing prices using feature engineering and cross validation'
                         label='Python, Sklearn, Pandas'
                     />
                     <CardItem
@@ -57,7 +63,7 @@ function Cards() {
                     <CardItem
                         src={require('../images/website.png')}
                         text="Vincent's Personal Website: The one you are currently on :)"
-                        label='React'
+                        label='ReactJS'
                         path='https://github.com/vincent-lin-0/vincentlin'
                     />
                 </ul>
